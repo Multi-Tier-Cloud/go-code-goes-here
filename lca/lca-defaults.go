33c1187ef08c2221ed6903cc152d724046d31be8
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-21 16:46:15 -0400

    1) Prevent allocator crash when incorrect or non-existant hash provided   - Root-cause is due to panic'ing when calling docker_driver.PullImage()     returns an error. It now properly terminates the stream and returns.   - Resolves #18 2) Beginning steps to move towards proper logging mechanism to help in    the debugging process.

--- lca/lca-defaults.go
+++ lca/lca-defaults.go
@@ -1,6 +1,8 @@
 package lca
 
 import (
+    "log"
+
     "github.com/libp2p/go-libp2p-core/protocol"
 
     "github.com/multiformats/go-multiaddr"
@@ -32,5 +34,8 @@ func init() {
 
     LCAAllocatorProtocolID = protocol.ID("/LCAAllocator/1.0")
     LCAAllocatorRendezvous = "QmQJRHSU69L6W2SwNiKekpUHbxHPXi57tWGRWJaD5NsRxS"
+
+    // Set up logging defaults
+    log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
 }
 
