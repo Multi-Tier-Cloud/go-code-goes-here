b98fd230391776a4192d75383efab3ddfc0355d3
Thomas Lin <t.lin@mail.utoronto.ca>
2020-06-21 13:33:23 -0400

    L4 proxy support for tunneling UDP over P2P connection

--- l4-proxy/udp-proxy.go
+++ l4-proxy/udp-proxy.go
@@ -5,13 +5,27 @@ import (
     "log"
     "net"
     "syscall"
+
+    "github.com/libp2p/go-libp2p-core/network"
+    "github.com/libp2p/go-libp2p-core/peer"
+    "github.com/libp2p/go-libp2p-core/protocol"
+
+    "github.com/t-lin/go-libp2p-gostream"
 )
 
+var udpTunnelProtoID = protocol.ID("/LCATunnelUDP/1.0")
+
 // UDP data forwarder
+// This function can be used to forward type a stream connection to a UDP
+// connection, and vice-versa. In some cases, the same socket is used to
+// receive from multiple clients. In such cases, writing to this UDP "Conn"
+// object requires an extra parameter, dstAddr. If dstAddr is not nil, then
+// consider dst to be type *net.UDPConn and use WriteTo() instead of Write().
+//
 // TODO: This function currently doesn't seem to end... the I/O functions do not
 //       appear to be returning errors. May need some way to timeout the
 //       connections so they don't keep consuming memory.
-func udpFwdData(src, dst *net.UDPConn, dstAddr net.Addr) {
+func udpFwdData(src, dst net.Conn, dstAddr net.Addr) {
     var err error
     var nBytes, nBytesW, n int
     buf := make([]byte, 0xffff) // 64k buffer
@@ -32,7 +46,11 @@ func udpFwdData(src, dst *net.UDPConn, dstAddr net.Addr) {
         data := buf[:nBytes]
 
         for nBytesW < nBytes {
-            n, err = dst.WriteTo(data, dstAddr)
+            if dstAddr != nil {
+                n, err = dst.(*net.UDPConn).WriteTo(data, dstAddr)
+            } else {
+                n, err = dst.Write(data)
+            }
             if err != nil {
                 if err == syscall.EINVAL {
                     log.Printf("Connection %s <=> %s closed", dst.LocalAddr(), dst.RemoteAddr())
@@ -50,24 +68,20 @@ func udpFwdData(src, dst *net.UDPConn, dstAddr net.Addr) {
 }
 
 // Demultiplex incoming packets and forward them to destination
-func udpServiceProxy(lConn *net.UDPConn, targetAddr string) {
+func udpServiceProxy(lConn *net.UDPConn, targetPeer peer.ID) {
     defer lConn.Close()
 
     var err error
 
-    // Resolve and open connection to destination service
-    rAddr, err := net.ResolveUDPAddr("udp", targetAddr)
-    if err != nil {
-        log.Printf("ERROR: Unable to resolve UDP target address %s\n%v\n", targetAddr, err)
-        return
-    }
-
     // Since there's no per-client UDP connection object, we'll need to do some
-    // manual demultiplexing. Create a separate outgoing UDP "connection" to the
+    // manual demultiplexing. Create a separate outgoing P2P stream to the
     // same target service per unique client.
-    client2RConn := make(map[string]*net.UDPConn)
+    //
+    // TODO: Use a NotifyBundle to watch for stream and connection close events,
+    //       and clean this data structure when they happen
+    client2RConn := make(map[string]net.Conn)
 
-    var rConn *net.UDPConn
+    var rConn net.Conn
     var exists bool
     var from net.Addr
     var nBytes, nBytesW, n int
@@ -91,11 +105,11 @@ func udpServiceProxy(lConn *net.UDPConn, targetAddr string) {
         if rConn, exists = client2RConn[from.String()]; !exists {
             log.Printf("New UDP conn: %s <=> %s\n", lConn.LocalAddr(), from)
 
-            rConn, err = net.DialUDP("udp", nil, rAddr)
+            p2pNode := manager.Host
+            rConn, err = gostream.Dial(p2pNode.Ctx, p2pNode.Host, targetPeer, udpTunnelProtoID)
             if err != nil {
-                log.Printf("ERROR: Unable to dial UDP target address %s\n%v\n",
-                    targetAddr, err)
-                continue // Or return? Other connections may be okay...
+                log.Printf("ERROR: Unable to dial target peer %s\n%v\n", targetPeer, err)
+                return
             }
             defer rConn.Close()
 
@@ -127,9 +141,9 @@ func udpServiceProxy(lConn *net.UDPConn, targetAddr string) {
 
 
 // Open UDP tunnel to service and open local UDP listening port
-func openUDPProxy(serviceAddr string) (string, error) {
+func openUDPProxy(servicePeer peer.ID) (string, error) {
     var listenAddr string
-    serviceKey := "udp://" + serviceAddr
+    serviceKey := "udp://" + string(servicePeer)
     if _, exists := serv2Fwd[serviceKey]; !exists {
         udpAddr, err := net.ResolveUDPAddr("udp", ctrlHost + ":") // choose port
         if err != nil {
@@ -146,10 +160,36 @@ func openUDPProxy(serviceAddr string) (string, error) {
             ListenAddr: listenAddr,
             udpWorker: udpServiceProxy,
         }
-        go serv2Fwd[serviceKey].udpWorker(conn, serviceAddr)
+        go serv2Fwd[serviceKey].udpWorker(conn, servicePeer)
     } else {
         listenAddr = serv2Fwd[serviceKey].ListenAddr
     }
 
     return listenAddr, nil
 }
+
+// Handler for udpTunnelProtoID (i.e. invoked at destination proxy)
+// Make a connection to the local service and forward data to/from it
+func udpTunnelHandler(stream network.Stream) {
+    lConn := gostream.NewConn(stream)
+    defer lConn.Close()
+
+    // Resolve and open connection to destination service
+    rAddr, err := net.ResolveUDPAddr("udp", servEndpoint)
+    if err != nil {
+        log.Printf("ERROR: Unable to resolve UDP target address %s\n%v\n", servEndpoint, err)
+        return
+    }
+
+    rConn, err := net.DialUDP("udp", nil, rAddr)
+    if err != nil {
+        log.Printf("ERROR: Unable to dial UDP target address %s\n%v\n", rAddr, err)
+        return
+    }
+    defer rConn.Close()
+
+    // Forward data in each direction
+    go udpFwdData(lConn, rConn, nil)
+    udpFwdData(rConn, lConn, nil)
+}
+
